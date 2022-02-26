91f1a13feb7fb7665e67218be1665081d4fe199a
Bekzat <95048127+bexterious@users.noreply.github.com>
2022-02-26 23:09:25 +0300

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -26,8 +26,8 @@ class Converter:
         except ValueError:
             raise ConvertionException(f'Не удалось обработать в количестве {amount}')
 
-        r = requests.get(f'https://api.exchangeratesapi.io/latest?base={quote_ticker}&symbols={base_ticker}')
-        total_base = json.loads(r.content)[keys[base]]
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
+        total_base = float(json.loads(r.content)[keys[quote]])
         z = total_base * amount
 
         return z
